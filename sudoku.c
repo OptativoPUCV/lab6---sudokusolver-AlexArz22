2455ff5d0bcad2f6d972a9a3bc0b4e42bbaa8305
Replit user <>
2023-05-05 03:40:55 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,7 +44,13 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-  
+
+  for(p=0;p<9;p++){
+      int i=3*(k/3) + (p/3) ;
+      int j=3*(k%3) + (p%3) ;
+      printf("%d ",n->sudo[i][j]);
+      if(p%3 == 2) printf("\n");
+  }
   for(int i=0; i<10; i++){
     int *a=calloc(10,sizeof(int));
     for(int j=0; j<10; j++){
@@ -61,12 +67,7 @@ int is_valid(Node* n){
     }
   }
   int k=4,p; 
-  for(p=0;p<9;p++){
-      int i=3*(k/3) + (p/3) ;
-      int j=3*(k%3) + (p%3) ;
-      printf("%d ",n->sudo[i][j]);
-      if(p%3 == 2) printf("\n");
-  }
+
   return 1;//valido
 }
 
